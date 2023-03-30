6e589631a943a3cdd9a47d1765f52d3f586a86ad
nahuellc <nahuelcoronel21@gmail.com>
2023-03-30 17:46:00 -0300

    Este es mi primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let variable1 = 3
+let variable2 = 4
+
+let resultado1 = variable1 + variable2
+
+let variable3 = 10
+let variable4 = 25
+
+let resultado2 = variable3 + variable4
+
+console.log(resultado1 + resultado2)
---
+++ tomadenotas.js
@@ -0,0 +1,3 @@
+// git init = Inicializa un repositorio dentro de la carpeta
+
+// cd .. = sale una carpeta hacia atrás.
