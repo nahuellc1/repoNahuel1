70f073b10635b897b51cde7ffa7238fd0990a6e1
nahuellc <nahuelcoronel21@gmail.com>
2023-04-07 19:54:13 -0300

    Formulario de Clase 2

--- tomadenotas.js
+++ tomadenotas.js
@@ -1,3 +1,12 @@
 // git init = Inicializa un repositorio dentro de la carpeta
 
 // cd .. = sale una carpeta hacia atrás.
+
+// git status = me muestra los archivos monitoreados o no en la carpeta
+// git add . = monitorea todos los archivos que se encuentren en la carpeta.
+
+// git commit -m "". m viene de mensaje. Si no se pone nos manda a otra carpeta.
+
+// git clone + url del repositorio de un compañero = 
+
+
